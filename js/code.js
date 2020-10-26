fa8bb65ad058d73bd9004943c132d21be8c166d1
Igrus97 <igrus97@gmail.com>
2020-10-26 15:55:27 +0300

    Скелет и канвас

---
+++ js/code.js
@@ -0,0 +1,202 @@
+// Код для создания снегопада на канвасе
+(() => {
+    const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+// создание снежинок
+let flakes = [],
+    canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    flakeCount = 855,
+    mX = -100,
+    mY = -100
+
+    canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+//создание снегопада
+function snow() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < flakeCount; i++) {
+        let flake = flakes[i],
+            x = mX,
+            y = mY,
+            minDist = 150,
+            x2 = flake.x,
+            y2 = flake.y;
+//дистанция снегопада
+        const dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+            dx = x2 - x,
+            dy = y2 - y;
+
+        if (dist < minDist) {
+            let force = minDist / (dist * dist),
+                xcomp = (x - x2) / dist,
+                ycomp = (y - y2) / dist,
+                deltaV = force / 2;
+
+            flake.velX -= deltaV * xcomp;
+            flake.velY -= deltaV * ycomp;
+
+        } else {
+            flake.velX *= .98;
+            if (flake.velY <= flake.speed) {
+                flake.velY = flake.speed
+            }
+            flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+        }
+
+        ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
+        flake.y += flake.velY;
+        flake.x += flake.velX;
+            
+        if (flake.y >= canvas.height || flake.y <= 0) {
+            reset(flake);
+        }
+
+
+        if (flake.x >= canvas.width || flake.x <= 0) {
+            reset(flake);
+        }
+
+        ctx.beginPath();
+        ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+    requestAnimationFrame(snow);
+};
+
+function reset(flake) {
+    flake.x = Math.floor(Math.random() * canvas.width);
+    flake.y = 0;
+    flake.size = (Math.random() * 3) + 2;
+    flake.speed = (Math.random() * 1) + 0.5;
+    flake.velY = flake.speed;
+    flake.velX = 0;
+    flake.opacity = (Math.random() * 0.5) + 0.3;
+}
+//инициализация
+function init() {
+    for (let i = 0; i < flakeCount; i++) {
+        let x = Math.floor(Math.random() * canvas.width),
+            y = Math.floor(Math.random() * canvas.height),
+            size = (Math.random() * 3) + 2,
+            speed = (Math.random() * 1) + 0.5,
+            opacity = (Math.random() * 0.5) + 0.3;
+
+        flakes.push({
+            speed: speed,
+            velY: speed,
+            velX: 0,
+            x: x,
+            y: y,
+            size: size,
+            stepSize: (Math.random()) / 30,
+            step: 0,
+            angle: 180,
+            opacity: opacity
+        });
+    }
+
+    snow();
+};
+//интерактивность
+canvas.addEventListener("mousemove", function(e) {
+    mX = e.clientX,
+    mY = e.clientY
+});
+
+init();
+
+//функция typewriter
+let TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    const elements = document.getElementsByClassName('typewrite');
+    for (let i=0; i<elements.length; i++) {
+        let toRotate = elements[i].getAttribute('data-type');
+        let period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
+
+
+//Код для динамической смены тайтла
+
+    //1. Берем контроль над тайтлом 
+    const prev_handler = window.onload;
+    window.onload = function() {
+        if (prev_handler) {
+            prev_handler()
+        }
+        const pageTitle = document.title;
+        const attentionMessage = '*Возвращайтесь!:)';
+        let blinkEvent = null;
+    // Евент лиснер чтобы детектить состояние страницы
+    document.addEventListener('visibilitychange', function(e) {
+    //console.log(document.hidden);
+        const isPageActive = !document.hidden;
+          if(!isPageActive){
+            blink();
+          }else {
+            document.title = pageTitle;
+            clearInterval(blinkEvent);
+          }
+        });
+        //блинк евент чтобы тайтл мерцал, привлекая внимание
+      //собственно функция на мерцание
+        function blink(){
+          blinkEvent = setInterval(function() {
+            if(document.title === attentionMessage){
+              document.title = pageTitle;
+            }else {
+              document.title = attentionMessage;
+            }
+          }, 100);
+        }
+      };
